f5386335bd334b475899c965016e02ef036b4e0a
blaclay <clayblackwelder@gmail.com>
2023-10-01 22:08:12 -0700

    finished

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,21 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        programs_list = []
+        programs = json.loads(self.get_response_body())
+        for program in programs:
+            programs_list.append(program)
+
+        return programs_list
+
+programs = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+requested = programs.get_response_body()
+print(requested)
+program_list = programs.load_json()
+print(program_list)
